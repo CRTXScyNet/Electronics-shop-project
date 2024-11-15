eaf4e3019540d42fc428d931d293c98d640b2071
CRTXScyNet <CRTXScyNet@gmail.com>
2024-11-15 15:40:49 +0500

    some refactors

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,13 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-import pytest
 from src.item import Item
-
 
 
 def test_calculate_total_price(get_item):
     item = get_item
 
     assert item.calculate_total_price() == 15000 * 21
+
 
 def test_apply_discount(get_item):
     item = get_item
@@ -20,4 +19,3 @@ def test_apply_discount(get_item):
     item.apply_discount()
 
     assert item.price == 15000 * Item.pay_rate
-
